c8b9e7f8c579c844acc3a8376f8eb29e7d983701
marco121942 <marco.sotomayor@tecsup.edu.pe>
2019-09-23 21:21:59 -0500

    first commit

---
+++ Data/SQLHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    static class SQLHelper
+    {
+        public static string Connection { get; } = "Data Source=DESKTOP-LNN14CJ\\SQLEXPRESS;Initial Catalog=neptuno;Integrated Security=True";
+
+        public static Int32 ExecuteNonQuery(String connectionString, String commandText,
+            CommandType commandType, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    // There're three command types: StoredProcedure, Text, TableDirect. The TableDirect  
+                    // type is only for OLE DB.   
+                    cmd.CommandType = commandType;
+                    cmd.Parameters.AddRange(parameters);
+
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+
+        }
+
+        public static Object ExecuteScalar(String connectionString, String commandText,
+            CommandType commandType, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(commandText, conn))
+                {
+                    cmd.CommandType = commandType;
+                    cmd.Parameters.AddRange(parameters);
+
+                    conn.Open();
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public static SqlDataReader ExecuteReader(String connectionString, String commandText,
+        CommandType commandType, params SqlParameter[] parameters)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            using (SqlCommand cmd = new SqlCommand(commandText, conn))
+            {
+                cmd.CommandType = commandType;
+                cmd.Parameters.AddRange(parameters);
+
+                conn.Open();
+                // When using CommandBehavior.CloseConnection, the connection will be closed when the  
+                // IDataReader is closed. 
+                SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                return reader;
+            }
+        }
+    }
+}
---
+++ Vista/Model/CategoriaModel.cs
@@ -0,0 +1,35 @@
+﻿using Business;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vista.Model
+{
+    public class CategoriaModel
+    {
+        public ObservableCollection<Categoria> categorias { get; set; }
+
+        public CategoriaModel()
+        {
+            var lista = (new BCategoria()).Get();
+            categorias = new ObservableCollection<Categoria>(lista);
+        }
+
+        public bool Insertar(Categoria categoria)
+        {
+            return (new BCategoria()).Insert(categoria);
+        }
+
+        public bool Actualizar(Categoria categoria)
+        {
+            return (new BCategoria()).Update(categoria);
+        }
+
+    }
+
+
+}
